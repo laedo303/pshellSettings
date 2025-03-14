e51ce28fe6bc185a9732e5cf52b70a85646debf9
laedo303 <laedo303@mail.ru>
2025-03-14 19:14:51 +0500

    add aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,12 @@ Set-Alias tt tree
 Set-Alias ll ls -force
 Set-Alias g git
 Set-Alias vim nvim
+Set-Alias vi nvim
+
 
 #Prompt
 #oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config 'C:\Users\anki\Documents\PowerShell\myprofile2.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\ankiHP\Documents\PowerShell\myprofile2.omp.json' | Invoke-Expression
 
 #Functions
 function whereis ($command) {
